14c4e73a2e9e084babde76ce182f374361298781
Joran Bergfeld <joranbergfeld@hotmail.com>
2022-12-07 12:06:09 +0100

    adds prometheus

--- src/main/java/com/joranbergfeld/httpreactivehttpservice/GreetingHandler.java
+++ src/main/java/com/joranbergfeld/httpreactivehttpservice/GreetingHandler.java
@@ -1,5 +1,6 @@
 package com.joranbergfeld.httpreactivehttpservice;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -7,12 +8,14 @@ import reactor.core.publisher.Mono;
 
 public class GreetingHandler {
 
+    @Timed(value = "http.handler.greeting.requests", histogram = true, percentiles = {0.95, 0.99}, extraTags = {"version", "v1"})
     public Mono<ServerResponse> greeting() {
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(new Greeting("Hi there!")));
     }
 
+    @Timed(value = "http.handler.long_greeting.requests", histogram = true, percentiles = {0.95, 0.99}, extraTags = {"version", "v1"})
     public Mono<ServerResponse> longGreeting() {
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON)
